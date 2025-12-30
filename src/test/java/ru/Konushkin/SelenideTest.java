850e8bced9132c089b0c9645d7c515863d3f45ed
KonushkinMaksim <mkonushkinqa@gmail.com>
2025-12-30 17:39:09 +0400

    add Lambda

---
+++ src/test/java/ru/Konushkin/SelenideTest.java
@@ -0,0 +1,55 @@
+package ru.Konushkin;
+
+import com.codeborne.selenide.Condition;
+import com.codeborne.selenide.logevents.SelenideLogger;
+import io.qameta.allure.selenide.AllureSelenide;
+import org.junit.jupiter.api.Test;
+import static com.codeborne.selenide.Selectors.withText;
+import static io.qameta.allure.Allure.step;
+import static org.openqa.selenium.By.linkText;
+import static com.codeborne.selenide.Selenide.$;
+import static com.codeborne.selenide.Selenide.open;
+
+public class SelenideTest {
+
+    private static final String REPOSITORY = "eroshenkoam/allure-example";
+    private static final int ISSUE = 80;
+    @Test
+    public void testIssueSearch(){
+        SelenideLogger.addListener("allure", new AllureSelenide());
+        open("https://github.com");
+        $(".search-input").click();
+        $("#query-builder-test").sendKeys("eroshenkoam/allure-example");
+        $("#query-builder-test").submit();
+        $(linkText("eroshenkoam/allure-example")).click();
+        $("issues-tab").click();
+        $(withText("#80")).should(Condition.exist);
+    }
+
+    @Test
+    public void testLambdaStep() {
+        SelenideLogger.addListener("allure", new AllureSelenide());
+
+        step("Открываем главную страницу", () -> {
+            open("https://github.com");
+        });
+        step("Ищем репозиторий " + REPOSITORY, () -> {
+            $(".search-input").click();
+            $("#query-builder-test").sendKeys(REPOSITORY);
+            $("#query-builder-test").submit();
+        });
+        step("Кликаем по ссылке репозитория " + REPOSITORY, () -> {
+            $(linkText(REPOSITORY)).click();
+        });
+        step("Открываем таб Issues", () -> {
+            $("#issues-tab").click();
+        });
+        step("Проверяем наличие Issue с номером " + ISSUE, () -> {
+            $(withText("#" + ISSUE)).should(Condition.exist);
+        });
+    }
+
+}
+
+
+
